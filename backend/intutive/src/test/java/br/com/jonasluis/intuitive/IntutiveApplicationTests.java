56441760e78851526fd1ef8677ae65443e6c7c56
jonasluis <jonasluis66@gmail.com>
2025-03-25 06:04:07 -0300

    :tada: Estrutura Inicial do Nivelamento

---
+++ backend/intutive/src/test/java/br/com/jonasluis/intuitive/IntutiveApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.jonasluis.intuitive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntutiveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
