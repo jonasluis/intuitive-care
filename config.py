711f1e4a7aab929bf496316c9312518a329570f4
jonasluis <jonasluis66@gmail.com>
2025-03-27 19:12:34 -0300

    chore: adiciona arquivos de configurações e dependencias do projeto

--- config.py
+++ config.py
@@ -4,8 +4,13 @@ import os
 # Carrega variáveis do arquivo .env
 load_dotenv()
 
-# Obtém a variável BASE_URL
+# URLs base
 BASE_URL = os.getenv("BASE_URL")
+FINANCIAL_DATA_URL = os.getenv("FINANCIAL_DATA_URL")
+OPERATORS_DATA_URL = os.getenv("OPERATORS_DATA_URL")
+
 # Diretórios
 PDF_FOLDER = "pdfs"
+FINANCIAL_DATA_FOLDER = "financial_data"
+OPERATORS_DATA_FOLDER = "operators_data"
 ZIP_FILE = "anexos.zip"
